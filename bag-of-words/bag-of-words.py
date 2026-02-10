8cc2ff1c24a72dc8f381ab2e922c482aa4c8a271
Harsh Kumar Chourasia <58362904+harshkumarchourasia@users.noreply.github.com>
2026-02-11 00:44:31 +0400

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -1,17 +1,8 @@
 import numpy as np
 
 def bag_of_words_vector(tokens, vocab):
-    """
-    Returns: np.ndarray of shape (len(vocab),), dtype=int
-    """
-   # Your code here
     vocab_size = len(vocab)
-    result = np.zeros(vocab_size, dtype=int)
-    word2id = dict()
-    for idx, word in enumerate(vocab):
-        word2id[word] = idx
-    for token in tokens:
-        if token not in word2id: continue
-        idx = word2id[token]
-        result[idx] += 1
-    return result
+    word2id = {word: i for i, word in enumerate(vocab)}
+
+    indices = [word2id[t] for t in tokens if t in word2id]
+    return np.bincount(indices, minlength=vocab_size)
